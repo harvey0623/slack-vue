2eb6bfad6d7e63ecefcdfca25934279d4bee6090
harvey <harvey@henghung.com>
2021-05-04 15:10:13 +0800

    update

--- src/components/Loading/index.vue
+++ src/components/Loading/index.vue
@@ -1,7 +1,7 @@
 <template>
    <div id="loading">
       <div class="loader">
-         <svg class="circular-loader" viewBox="25 25 50 50" >
+         <svg class="circular-loader" viewBox="25 25 50 50">
             <circle class="loader-path" cx="50" cy="50" r="20"></circle>
          </svg>
       </div>
